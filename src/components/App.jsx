81e31dbaa1d98f56f6162576d29f18ab4050aeb8
CHH6IYGS <bahaa.kiali@diva-e.com>
2021-03-02 20:50:01 +0100

    add bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,13 @@ import H1 from './atoms/H1';
 
 const App = () => (
   <div>
-    <H1
-      text='Welcome to React App'
-    />
+    <div className='row'>
+      <div className='col-xs-12'>
+        <H1
+          text='Welcome to React App'
+        />
+      </div>
+    </div>
   </div>
 );
 
